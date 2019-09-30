82d56930143950472d245b058f9cc160d9bb5bd8
AFa0304 <c805237@gmail.com>
2019-09-30 21:03:35 +0800

    update

---
+++ build/precache-manifest.adaa77f9b0f62a05d818e089b9ba0ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f732befa7fd5d458f2c18375da03625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31e412f983ca6c0ec0d4",
+    "url": "/static/css/main.8f1fc6b6.chunk.css"
+  },
+  {
+    "revision": "7dd085355146fc39130b",
+    "url": "/static/js/2.bc903f66.chunk.js"
+  },
+  {
+    "revision": "31e412f983ca6c0ec0d4",
+    "url": "/static/js/main.d2bbc9f2.chunk.js"
+  },
+  {
+    "revision": "ccfd26d9bb0b6e26778a",
+    "url": "/static/js/runtime-main.b14cbc69.js"
+  }
+]);
